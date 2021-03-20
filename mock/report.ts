572a87e1401be1e997e2edbb2ce1b0d51a4be1cf
NoraHito <834053207@qq.com>
2021-03-20 14:22:47 +0800

    修正交换机故障上报mock删除接口返回体

--- mock/report.ts
+++ mock/report.ts
@@ -4,5 +4,5 @@ export default {
   'GET /api/switch-fault-report/list': base('data|1-10', [reportSwitchFault]),
   'POST /api/switch-fault-report/add': base('data', reportSwitchFault),
   'POST /api/switch-fault-report/update': base('data', reportSwitchFault),
-  'POST /api/switch-fault-report/delete': base('data', reportSwitchFault),
+  'POST /api/switch-fault-report/delete': base('data', {}),
 };
