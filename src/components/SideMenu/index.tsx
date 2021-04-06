edb476b8231263521096e04384431ce12d81ef26
NoraHito <834053207@qq.com>
2021-04-06 14:19:11 +0800

    抽取菜单

---
+++ src/components/SideMenu/index.tsx
@@ -0,0 +1,97 @@
+import { Menu } from 'antd';
+import './index.scss';
+
+import { History, Location } from 'umi';
+
+import { useState, useEffect, FC } from 'react';
+import { routeInterface } from 'route';
+
+const { SubMenu } = Menu;
+
+const SideMenu: FC<{ history: History; pcRoutes?: routeInterface.route[] }> = (
+  props,
+) => {
+  const { history, pcRoutes } = props;
+  // 当前选中的菜单
+  const [currentMenuKey, setCurrentMenuKey] = useState([
+    history.location.pathname,
+  ]);
+  // 当前展开的菜单
+  const [currentOpenSubMenuKey, setCurrentOpenSubMenuKey] = useState<any[]>([]);
+
+  // 根据路径获取需要展开的菜单
+  const getMenuNeedOpen = (
+    path: string,
+    routes: routeInterface.route[],
+  ): any => {
+    for (let route of routes) {
+      if (path === route.path) {
+        if (route.extraOpt?.sub || route.extraOpt?.default)
+          return [route.extraOpt?.key];
+        return [route.extraOpt?.parentMenu] ?? [];
+      }
+      if (route.routes && route.routes.length > 0) {
+        let result = getMenuNeedOpen(path, route.routes);
+        if (result.length > 0) return result;
+      }
+    }
+    return [];
+  };
+
+  useEffect(() => {
+    // 首次进入展开对应菜单
+    setCurrentOpenSubMenuKey(
+      getMenuNeedOpen(location.pathname, pcRoutes || []),
+    );
+    // 监听路由变化,更新菜单
+    const unlisten = history.listen((location: Location) => {
+      setCurrentMenuKey([location.pathname]);
+    });
+    // 组件销毁前解除监听
+    return () => {
+      unlisten();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="logo" />
+      <Menu
+        theme="light"
+        onSelect={({ key: path }: { key: any }) => history.push(path)}
+        selectedKeys={currentMenuKey}
+        openKeys={currentOpenSubMenuKey}
+        onOpenChange={(openKeys) => setCurrentOpenSubMenuKey(openKeys)}
+        mode="inline"
+      >
+        {pcRoutes
+          ? pcRoutes.map((route) => {
+              if (route.extraOpt?.hidden || !route.extraOpt?.menu) return;
+              if (
+                route.extraOpt?.sub &&
+                route.routes &&
+                route.routes.length > 0
+              )
+                return (
+                  <SubMenu key={route.path} title={route.extraOpt?.name}>
+                    {route.routes.map((childRoute) =>
+                      childRoute.extraOpt?.hidden ? null : (
+                        <Menu.Item key={childRoute.path}>
+                          {childRoute.extraOpt?.name}
+                        </Menu.Item>
+                      ),
+                    )}
+                  </SubMenu>
+                );
+              else
+                return (
+                  <Menu.Item key={route.path}>{route.extraOpt?.name}</Menu.Item>
+                );
+            })
+          : null}
+      </Menu>
+    </>
+  );
+};
+
+export default SideMenu;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,13 @@ import { History, Location } from 'umi';
 import { useState, useEffect, useMemo, useContext } from 'react';
 import { routeInterface } from 'route';
 
+import SideMenu from '@/components/SideMenu';
+
 // 抽取出 PC 端的路由
-const [, , ,pcRoute] = desktopRoute;
+const [, , , pcRoute] = desktopRoute;
 const { routes: pcRoutes } = pcRoute;
 
 const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
 
 // 根据路径获取指定路由列表中的路由全名
 const getRouteNamePath = (
@@ -30,50 +31,10 @@ const getRouteNamePath = (
   return '_null';
 };
 
-// 根据路径获取需要展开的菜单
-const getMenuNeedOpen = (path: string, routes: routeInterface.route[]): any => {
-  for (let route of routes) {
-    if (path === route.path) {
-      if (route.extraOpt?.sub || route.extraOpt?.default)
-        return [route.extraOpt?.key];
-      return [route.extraOpt?.parentMenu] ?? [];
-    }
-    if (route.routes && route.routes.length > 0) {
-      let result = getMenuNeedOpen(path, route.routes);
-      if (result.length > 0) return result;
-    }
-  }
-  return [];
-};
-
 const index = (props: any) => {
   const {
-    routes,
     history,
   }: { routes: routeInterface.route[]; history: History } = props;
-  // 当前选中的菜单
-  const [currentMenuKey, setCurrentMenuKey] = useState([
-    history.location.pathname,
-  ]);
-  // 当前展开的菜单
-  const [currentOpenSubMenuKey, setCurrentOpenSubMenuKey] = useState<any[]>([]);
-
-  useEffect(() => {
-    // 首次进入展开对应菜单
-    setCurrentOpenSubMenuKey(
-      getMenuNeedOpen(location.pathname, pcRoutes || []),
-    );
-    // 监听路由变化,更新左侧菜单
-    const unlisten = history.listen(
-      (location: Location, action: History['action']) => {
-        setCurrentMenuKey([location.pathname]);
-      },
-    );
-    // 组件销毁前解除监听
-    return () => {
-      unlisten();
-    };
-  }, []);
 
   // 面包屑
   const breadCrumbDataList = useMemo(
@@ -88,43 +49,7 @@ const index = (props: any) => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider theme="light" className="site-layout-sider">
-        <div className="logo" />
-        <Menu
-          theme="light"
-          onSelect={({ key: path }: { key: any }) => history.push(path)}
-          selectedKeys={currentMenuKey}
-          openKeys={currentOpenSubMenuKey}
-          onOpenChange={(openKeys) => setCurrentOpenSubMenuKey(openKeys)}
-          mode="inline"
-        >
-          {pcRoutes
-            ? pcRoutes.map((route) => {
-                if (route.extraOpt?.hidden || !route.extraOpt?.menu) return;
-                if (
-                  route.extraOpt?.sub &&
-                  route.routes &&
-                  route.routes.length > 0
-                )
-                  return (
-                    <SubMenu key={route.path} title={route.extraOpt?.name}>
-                      {route.routes.map((childRoute) =>
-                        childRoute.extraOpt?.hidden ? null : (
-                          <Menu.Item key={childRoute.path}>
-                            {childRoute.extraOpt?.name}
-                          </Menu.Item>
-                        ),
-                      )}
-                    </SubMenu>
-                  );
-                else
-                  return (
-                    <Menu.Item key={route.path}>
-                      {route.extraOpt?.name}
-                    </Menu.Item>
-                  );
-              })
-            : null}
-        </Menu>
+        <SideMenu pcRoutes={pcRoutes} history={history} />
       </Sider>
       <Layout className="site-layout">
         <Header className="site-layout-header" style={{ padding: 0 }} />
